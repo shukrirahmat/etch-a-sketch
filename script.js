70b355b5396005e199f30ce77c998f06b10c117e
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-02-19 16:39:54 +0800

    Add a couple of simple features
    User can choose the size of the grid, chhose either want to color in black, or random color, erasing and resetting the grid.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let size = 16;
+let colorMode = "black";
 const container = document.querySelector('#container');
-addGrid(size);
+addGrid(16);
+addButton();
 
 function addGrid(size) {
     let i = 0;
     while (i < size) {
-        let column = document.createElement('div');
+        const column = document.createElement('div');
         column.classList.add("column");
         container.appendChild(column);
         i++;
@@ -15,7 +16,7 @@ function addGrid(size) {
     allColumn.forEach((column) => {
         let j = 0;
         while (j < size) {
-            let grid = document.createElement('div');
+            const grid = document.createElement('div');
             grid.classList.add("grid");
             grid.addEventListener('mouseover',colorGrid);
             column.appendChild(grid);
@@ -25,5 +26,57 @@ function addGrid(size) {
 }
 
 function colorGrid() {
-    this.classList.add("colored");
+    if (colorMode == "black") {
+        this.style.backgroundColor = "black";
+    } else if (colorMode == "color") {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else {
+        this.style.backgroundColor = "white"; 
+    }
+}
+
+function addButton() {
+    const topdiv = document.querySelector('#top');
+    const sizebtn = document.createElement('button');
+    sizebtn.textContent = "Change Size";
+    sizebtn.addEventListener('click', changeSize);
+
+    const resetbtn = document.createElement('button');
+    resetbtn.textContent = "Reset";
+    resetbtn.addEventListener('click', reset);
+
+    const blackbtn = document.createElement('button');
+    blackbtn.textContent = "Black";
+    blackbtn.addEventListener('click', () => colorMode = "black");
+
+    const whitebtn = document.createElement('button');
+    whitebtn.textContent = "White";
+    whitebtn.addEventListener('click', () => colorMode = "white");
+
+    const colorbtn = document.createElement('button');
+    colorbtn.textContent = "Color";
+    colorbtn.addEventListener('click', () => colorMode = "color");
+
+    topdiv.appendChild(sizebtn);
+    topdiv.appendChild(resetbtn);
+    topdiv.appendChild(blackbtn);
+    topdiv.appendChild(whitebtn);
+    topdiv.appendChild(colorbtn);
+}
+
+function changeSize() {
+    let newSize = prompt("Enter size : (number of squares per side, max=100)", 16);
+    if (newSize > 0 && newSize <= 100) {
+        const allColumn = document.querySelectorAll(".column");
+        allColumn.forEach(column => column.remove());
+        addGrid(newSize);
+    }
+}
+
+function reset() {
+    const allGrid = document.querySelectorAll(".grid");
+    allGrid.forEach(grid => grid.style.backgroundColor = "white");  
 }
